32ca41772f79d6f8ac29bfa6dd58f6d2a30979c6
Ramjius <ramgmuhsin@gmail.com>
2023-07-13 07:42:02 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+ *main - the main function of the program
+ *
+ *Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return(0);
+}

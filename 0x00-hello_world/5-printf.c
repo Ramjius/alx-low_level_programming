4f0c77e2c927b7d1a0438cd9afeb8bc7c3a6f6a2
Ramjius <ramgmuhsin@gmail.com>
2023-07-13 08:03:52 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+ * main - the main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

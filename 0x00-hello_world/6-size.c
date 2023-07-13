dfd2fb98cfd19a10b6bb0003306594e85454c951
Ramjius <ramgmuhsin@gmail.com>
2023-07-13 08:41:46 -0700

    Sizes of environment

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
 	printf("Size of float: %lu byte(s)", sizeof(float));
-	return ();
+	return (0);
 }
 
 

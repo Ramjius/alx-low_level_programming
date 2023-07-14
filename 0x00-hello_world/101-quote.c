b94ecbcd32d2118cf3b6ee85fa5f86bedf4b59f2
Ramjius <ramgmuhsin@gmail.com>
2023-07-14 13:36:06 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
- * main - the main function prints "and that piece of art is useful"" - Dora Korpar, 2015-10-19\n"
- * Return : Always 1 (Success)
+ * main - main prints "and that piece of art is useful"...
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return(1);
+	return (1);
 
 }

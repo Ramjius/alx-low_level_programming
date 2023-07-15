b2442c97128078fca6fd8fa497880608bb6aa826
Ramjius <ramgmuhsin@gmail.com>
2023-07-15 02:19:16 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+* main - pints base 10 numbers
+* Return: Always 0
+*/
+int main(void)
+{
+	/* Define temporary variables */
+	int value;
+
+	/* For loop to generate the value of range 0 to 10 */
+	for (value = 0; value < 10; value++)
+	{
+	/* Display result */
+	printf("%d", value);
+	}
+	printf("\n");
+
+	return (0);
+}

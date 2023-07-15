5291aba6c4c739f4d2b635fc8be45bbb59b4148d
Ramjius <ramgmuhsin@gmail.com>
2023-07-14 19:23:54 -0700

    Alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - outputs a-z
+* Return: Always 0
+*
+*/
+
+int main(void)
+
+{
+	char i = 97;
+
+	for (; i < 123; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

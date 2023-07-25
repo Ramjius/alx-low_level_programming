1ca3ea71c832dd189d72ff8ab23e4a6f79c2aaca
Ramjius <ramgmuhsin@gmail.com>
2023-07-24 17:22:51 -0700

    Print Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_rev - reverse print
+* @s: string being printed
+* Return: 0 (Successful)
+*/
+
+void print_rev(char *s)
+{
+	int str_length = 0;
+	int m;
+
+	while (*s != '\0')
+	{
+		str_length++;
+		s++;
+	}
+	s--;
+	for (m = str_length; m > 0; m--)
+	{
+		_putchar(*s);
+		s--;
+	}
+}

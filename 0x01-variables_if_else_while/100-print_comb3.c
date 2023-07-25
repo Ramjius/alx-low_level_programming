b4ae51b3f840e1c315342508fc857504c1256d04
Ramjius <ramgmuhsin@gmail.com>
2023-07-24 22:17:23 -0700

    all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,25 +5,28 @@
 * Return: 0 Always (Success)
 */
 
-int main()
+int main(void)
 {
 	int i;
 	int j;
 
-	for (i = 0; i <= 9; i++) {
-		for (j = i + 1; j <= 9; j++) {
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
 		putchar(i + '0');
 		putchar(j + '0');
-		if (i != 8 || j != 9) {
+			if (i != 8 || j != 9)
+			{
 			putchar(',');
 			putchar(' ');
-            	}
-		else
-		{
-		putchar('\n');
+			}
+			else
+			{
+			putchar('\n');
+			}
 		}
-        }
-    }
+	}
 
-    return 0;
+	return (0);
 }

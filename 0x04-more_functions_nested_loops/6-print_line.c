06a99451648c600bdc572a5e91b23a8d5d53a1ba
Ramjius <ramgmuhsin@gmail.com>
2023-07-27 02:20:52 -0700

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_line - function prints a line on screen
+* Return: void
+* @n: input
+*/
+
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < n; x++)
+	{
+	_putchar(95);
+	}
+	_putchar('\n');
+	}
+}

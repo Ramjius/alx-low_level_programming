96a88f407c74aeacbdd51809de959de92b3e014a
Ramjius <ramgmuhsin@gmail.com>
2023-07-29 15:25:44 -0700

    a function that prints an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+* print_positive_number - prints positive integer
+* @n: input
+* Return: void
+*/
+
+void print_positive_number(int n)
+{
+	if (n == 0)
+	return;
+	print_positive_number(n / 10);
+	_putchar('0' + (n % 10));
+}
+
+/**
+* print_number - prints a 0 or -ve integer
+* @n: input
+* Return: void
+*/
+
+void print_number(int n)
+{
+	if (n == 0)
+	{
+	_putchar('0');
+	}
+	else if (n < 0)
+	{
+	_putchar('-');
+	print_positive_number(-n);
+	}
+	else
+	{
+	print_positive_number(n);
+	}
+_putchar('\n');
+}

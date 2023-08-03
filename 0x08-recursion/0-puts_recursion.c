9bcc615bc6af7da318279015915f6c5d5615f187
Ramjius <ramgmuhsin@gmail.com>
2023-08-02 17:27:07 -0700

    0-Task - prints a string, followed by a new line.

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,13 +3,15 @@
 /**
 * _puts_recursion - prints string then newline
 * @s: input string
+* Return: nothing.
 */
- 
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
 	_putchar(*s);
 	s++;

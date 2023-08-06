59c43af70e2e0c191f0135536b56c50ae8ae2d3e
Ramjius <ramgmuhsin@gmail.com>
2023-08-06 00:34:12 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,19 +14,18 @@ int main(void)
 	for (p = 0; p <= 98; p++)
 	{
 
-	for (q = p + 1; q <= 99; q++)
-	{
-	putchar((p / 10) + '0');
-	putchar((p % 10) + '0');
-	putchar(' ');
-	putchar((q / 10) + '0');
-	putchar((q % 10) + '0');
-	p++;
-	if ((p == 98 && q == 99))
-	continue;
-	putchar(',');
-	putchar(' ');
-	}
+		for (q = p + 1; q <= 99; q++)
+		{
+		putchar((p / 10) + '0');
+		putchar((p % 10) + '0');
+		putchar(' ');
+		putchar((q / 10) + '0');
+		putchar((q % 10) + '0');
+		if (p == 98 && q == 99)
+		continue;
+		putchar(',');
+		putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
